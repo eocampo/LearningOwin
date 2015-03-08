c0e0b07064e4c042c93b529170dc68981d7ed329
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2015-03-08 15:04:33 -0600

    Initial commit.

---
+++ OwinHelloWorld/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Owin;
+
+namespace OwinHelloWorld
+{
+    public class Startup
+    {        
+        public void Configuration(IAppBuilder app) {            
+            app.Run(context => {
+                if (context.Request.Path.Value == "/fail")
+                    throw new Exception("Random exception");
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
