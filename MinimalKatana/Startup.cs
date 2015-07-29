1d16572d696315db08a0cd6137c732d4172669c7
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2015-07-29 09:57:02 -0500

    Previous commit hasn't the .sln file updated!

---
+++ MinimalKatana/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MinimalKatana
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class Startup
+    {
+        public AppFunc Configuration() {
+            return async env => {
+                var writer = new StreamWriter((Stream)env["owin.ResponseBody"]);
+                await writer.WriteAsync("Hello World!");
+                await writer.FlushAsync();
+            };
+        }
+    }
+}
